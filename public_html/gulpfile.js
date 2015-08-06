9097bc022e44edb5d0cffadf88255617cf017afa
J. Tran <j.yn.tran@gmail.com>
2015-08-05 23:57:45 -0400

    Styling. Info box animation.

--- public_html/gulpfile.js
+++ public_html/gulpfile.js
@@ -2,9 +2,31 @@
 var gulp = require('gulp'); 
 
 // include plug-ins
+var browserSync = require('browser-sync').create();
 var changed = require('gulp-changed');
 var minifyHTML = require('gulp-minify-html');
 var sass = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch("./app/style/**/*.scss", ['sass']);
+    gulp.watch("./app/**/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./app/style/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./app/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
 
 // minify new or changed HTML pages
 gulp.task('minify-html', function() {
@@ -18,13 +40,8 @@ gulp.task('minify-html', function() {
 });
 
 // compile sass
-gulp.task('sass', function () {
-  gulp.src('./app/style/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/css'))
-});
-
-// default task
-gulp.task('default', function() {
-  gulp.watch('./app/style/**/*.scss', ['sass']);
-});
+// gulp.task('sass', function () {
+//   gulp.src('./app/style/**/*.scss')
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(gulp.dest('./app/css'))
+// });
