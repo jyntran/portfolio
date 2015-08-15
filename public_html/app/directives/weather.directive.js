c65cfc3438ec8bac82c0ba9e6e2521f09c3e262e
jyntran <j.yn.tran@gmail.com>
2015-08-15 16:53:39 +0300

    Added weather directive

---
+++ public_html/app/directives/weather.directive.js
@@ -0,0 +1,13 @@
+(function(){
+    'use strict';
+
+    angular.module('app')
+    .directive('weather', ['ApiService', function(ApiService){
+        return function(scope, elem, attr) {
+            ApiService.getWeather()
+            .then(function(resp){
+                scope.weather = resp.weather[0];
+            })
+        }
+    }])
+})();
--- public_html/app/directives/weatherBg.directive.js
+++ public_html/app/directives/weatherBg.directive.js
@@ -3,39 +3,40 @@
 
     angular
     .module('app')
-    .directive('weatherBg', ['ApiService', function(ApiService){
+    .directive('weatherBg', function(){
         return {
             restrict: 'A',
             link: function(scope, elem, attr) {
-                ApiService.getWeather()
-                .then(function(resp){
-                    scope.weather = resp.weather[0].main.toLowerCase();
-                    var file = '';
-                    if (~scope.weather.indexOf('rain')
-                        || (~scope.weather.indexOf('drizzle'))){
-                        file = 'rain';
-                    } else if (~scope.weather.indexOf('thunderstorm')){
-                        file = 'thunderstorm';
-                    } else if (~scope.weather.indexOf('snow')
-                        || ~scope.weather.indexOf('hail')
-                        || ~scope.weather.indexOf('sleet')){
-                        file = 'snow';
-                    } else if (~scope.weather.indexOf('clouds')
-                        || ~scope.weather.indexOf('storm')) {
-                        file = 'clouds';
-                    } else {
-                        if (scope.hour <= 5 || scope.hour >= 22)
-                            file = 'night';
-                        else if (scope.hour >= 6 && scope.hour <= 9) 
-                            file = 'sunrise'
-                        else if (scope.hour >= 18 && scope.hour <= 21) 
-                            file = 'sunset'
-                        else
-                            file = 'clear';
+                scope.$watch('weather', function(weather){
+                    if (weather) {
+                        var main = weather.main.toLowerCase();
+                        var file = '';
+                        if (~main.indexOf('rain')
+                            || (~main.indexOf('drizzle'))){
+                            file = 'rain';
+                        } else if (~main.indexOf('thunderstorm')){
+                            file = 'thunderstorm';
+                        } else if (~main.indexOf('snow')
+                            || ~main.indexOf('hail')
+                            || ~main.indexOf('sleet')){
+                            file = 'snow';
+                        } else if (~main.indexOf('clouds')
+                            || ~main.indexOf('storm')) {
+                            file = 'clouds';
+                        } else {
+                            if (scope.hour <= 5 || scope.hour >= 22)
+                                file = 'night';
+                            else if (scope.hour >= 6 && scope.hour <= 9) 
+                                file = 'sunrise'
+                            else if (scope.hour >= 18 && scope.hour <= 21) 
+                                file = 'sunset'
+                            else
+                                file = 'clear';
+                        }
+                        elem.css('background-image', 'url(/app/style/img/weather_'+file+'.jpg)');
                     }
-                    elem.css('background-image', 'url(/app/style/img/weather_'+file+'.jpg)');
                 })
             }
         }
-    }]);
+    });
 })();
