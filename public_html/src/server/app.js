dcec1316d466d32bdfb5ee78c8a78822a95d50fe
jyntran <j.yn.tran@gmail.com>
2015-08-18 00:42:04 +0300

    Cleaned up app.js. Deployment works.

--- public_html/src/server/app.js
+++ public_html/src/server/app.js
@@ -1,26 +1,31 @@
-// BASE SETUP
-// ==============================================
-
-'use strict';
-
 var express = require('express');
 var connect = require('connect');
+var path = require('path');
 var bodyParser = require('body-parser');
+var requireDir = require('require-dir');  
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+var port    =   process.env.PORT || 8080;
+
 var app     = express();
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-
-var port    =   process.env.PORT || 8080;
-var path = require('path');
  
-var nodemailer = require('nodemailer');
-var smtpTransport = require('nodemailer-smtp-transport');
+app.use(express.static('./build/'));
+app.use(express.static('./src/client/'));
+app.use(express.static('./'));
+
+var appDir = path.join(process.cwd(), './build/');
 
 var generator = require('xoauth2').createXOAuth2Generator({
     user: 'j.yn.tran@gmail.com',
-    //...
+    clientId: '93155087723-cn6iuhqh3hef4trt5e8fndt32pv6glbr.apps.googleusercontent.com',
+    clientSecret: 'o2MKas4Zk3Y7fLN7BWAupbPB',
+    refreshToken: '1/Bf12MxJE8bpBtquWvgIEcQwJ5tXfE_hu2lYQUxgCcCM',
+    accessToken: 'ya29.0gFDXP_zMbO3a96gnAYUqCfCHbOD0zHmUEt0zZc6jLgmDJjIaGUHbAwCoeBh2N2CcG28'
 });
 
 generator.on('token', function(token){
@@ -35,34 +40,17 @@ var transporter = nodemailer.createTransport(smtpTransport({
     }
 }));
 
-var appDir = path.join(process.cwd(), './build/');
-
-app.use(express.static('./build/'));
-app.use(express.static('./src/client/'));
-app.use(express.static('./'));
-
-// ROUTES
-// ==============================================
-
-// get an instance of router
 var router = express.Router();
 
-// route middleware that will happen on every request
 router.use(function(req, res, next) {
-
-    // log each request to the console
     console.log(req.method, req.url);
-
-    // continue doing what we were doing and go to the route
     next(); 
 });
 
-// home page route 
 router.get('/', function(req, res) {
     res.sendFile(appDir + 'index.html');  
 });
 
-// contact
 router.post('/contact', function(req, res) {
     var data = req.body.form;
     var mailOptions = {
@@ -87,11 +75,8 @@ router.post('/contact', function(req, res) {
     })
 });
 
-// apply the routes to our application
 app.use('/', router);
 
-// START THE SERVER
-// ==============================================
 app.listen(port, function(){
     console.log('--- SERVER ---');
     console.log('Magic happens on port ' + port);
