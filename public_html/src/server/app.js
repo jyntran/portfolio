b1ce1cbd994b3beee48742deeb7160b62afe11e1
jyntran <j.yn.tran@gmail.com>
2015-08-18 05:40:09 +0300

    Added error handlers

--- public_html/src/server/app.js
+++ public_html/src/server/app.js
@@ -18,10 +18,26 @@ app.use(express.static('./'));
 
 app.use('/', routes);
 
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    console.log(err);
+    res.status(err.status).send({error: err});
+  });
+}
+
+app.use(function (req, res, next){
+    res.status(404).send();
+})
+
+app.use(function(err, req, res, next) {
+    console.log(err)
+  res.sendStatus(err.status || 500);
+});
+
 app.listen(port, function(){
     console.log('--- SERVER ---');
     console.log('Magic happens on port ' + port);
-    console.log('env = ' + app.get('env') +
-        '\n__dirname = ' + __dirname  +
-        '\nprocess.cwd = ' + process.cwd());    
+    console.log('    env = ' + app.get('env') +
+        '\n    __dirname = ' + __dirname  +
+        '\n    process.cwd = ' + process.cwd());    
 });
