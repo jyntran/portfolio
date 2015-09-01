a0813a541062f7db4783a7cc352c19278f2af060
jyntran <j.yn.tran@gmail.com>
2015-09-01 06:11:49 +0300

    Rehaul of workboxes. Some tweaking in resume view.

--- public_html/src/client/app/directives/resize.directive.js
+++ public_html/src/client/app/directives/resize.directive.js
@@ -5,16 +5,24 @@
     .directive('resize', function($window){
         return {
             link: function(scope) {
+                scope.resp = null;
+                
                 angular.element($window).on('resize', function(e) {
-                    // Namespacing events with name of directive + event to avoid collisions
                     var w = $window,
                         x = w.innerWidth,
                         y = w.innerHeight;
-                    scope.$broadcast('resize::resize', {
-                        innerWidth: x,
-                        innerHeight: y
-                    });
-              });
+                    checkWidth(x);
+                });
+
+                function checkWidth(x) {
+                    if (x >= 568 && x < 768)
+                        scope.resp = 'sm';
+                    else if (x >= 768 && x < 1024)
+                        scope.resp = 'md';
+                    else
+                        scope.resp = 'lg';
+                    return scope.resp;
+                }
             }
         }
     })
